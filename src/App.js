9c48417472266f0a25c96e4f4dee1b271863108b
keaton <keatonrgallagher@gmail.com>
2020-10-09 10:52:46 -0600

    General update pattern for D3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { select } from 'd3'
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([25, 30, 45, 60, 20])
+  const svgRef = useRef();
+
+  useEffect(() => {
+    const svg = select(svgRef.current)
+    // Select all circles you find in my svg and sync them with my data 
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join(
+        enter => enter
+          .append('circle')
+          .attr("stroke", "teal")
+          .attr("fill", "lightgreen"),
+        update => update
+          .attr("class", "update")
+          .attr("fill", "red") // Define what you want to do with your elements
+        // Update and sets class
+      ).attr("r", v => v) // Create r is for radius
+      .attr("cx", v => v * 2) // x coordinate 
+      .attr("cy", v => v * 2)// y coordinate
+
+  }, [data])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <svg ref={svgRef}></svg>
+      <br />
+      <button onClick={() => setData(data.map(v => v + 5))}>
+        Update Data
+      </button>
+      <button onClick={() => setData(data.filter(v => v < 35))}>
+        Filter Data
+      </button>
+    </React.Fragment>
   );
 }
 

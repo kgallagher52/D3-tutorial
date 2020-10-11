ae3d1592a68b85d18260368d775551a6a25a6d45
keaton <keatonrgallagher@gmail.com>
2020-10-10 19:23:31 -0600

    Added y and x axis

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { select } from 'd3'
+import {
+  axisBottom,
+  curveCardinal,
+  select,
+  line,
+  scaleLinear,
+  axisRight
+} from 'd3'
 
 const App = () => {
-  const [data, setData] = useState([25, 30, 45, 60, 20])
+  const [data, setData] = useState([25, 76, 45, 60, 20, 60, 30])
   const svgRef = useRef();
+
+
 
   useEffect(() => {
     const svg = select(svgRef.current)
-    // Select all circles you find in my svg and sync them with my data 
+
+    const xScale = scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, 300])
+    const yScale = scaleLinear()
+      .domain([0, 150])
+      .range([150, 0])
+
+    const xAxis = axisBottom(xScale).ticks(data.length).tickFormat(index => index + 1); // Creating x axis ticks 
+    svg.select('.x-axis')
+      .style("transform", "translateY(150px)")
+      .call(xAxis)
+
+    const yAxis = axisRight(yScale); // Creating y axis ticks 
+    svg.select('.y-axis')
+      .style("transform", "translateX(300px)")
+      .call(yAxis)
+
+    const myLine = line()
+      .x((value, index) => xScale(index))
+      .y(yScale)
+      .curve(curveCardinal)
+    // Wrapped in another array so it does not generate multiple lines rather then one
     svg
-      .selectAll("circle")
-      .data(data)
-      .join(
-        enter => enter
-          .append('circle')
-          .attr("stroke", "teal")
-          .attr("fill", "lightgreen"),
-        update => update
-          .attr("class", "update")
-          .attr("fill", "red") // Define what you want to do with your elements
-        // Update and sets class
-      ).attr("r", v => v) // Create r is for radius
-      .attr("cx", v => v * 2) // x coordinate 
-      .attr("cy", v => v * 2)// y coordinate
+      .selectAll(".line")
+      .data([data])
+      .join("path")
+      .attr("class", "line")
+      .attr("d", myLine)
+      .attr("fill", "none")
+      .attr("stroke", "teal")
 
   }, [data])
 
   return (
     <React.Fragment>
-      <svg ref={svgRef}></svg>
+      <svg ref={svgRef}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
       <br />
       <button onClick={() => setData(data.map(v => v + 5))}>
         Update Data
